15cb40c63b791c2ec023342eb5e28e2e7b6935bf
Gliz23 <geaakoto@st.ug.edu.gh>
2024-04-27 16:26:48 +0000

    Text5(T5) file added

--- src/Components/BG Templates/Template5/T5.jsx
+++ src/Components/BG Templates/Template5/T5.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import './T5.css'
 
-const T5 = () => {
+const T5 = (props) => {
+
   return (
-    <div>
-      <h1>T5</h1>
+    <div className='template5' id='template5'>
+      <img src={props.imageUrl} alt="Your Image" />
+      <div className="text-container">
+
+        <div className="text-box1">
+            <p>{props.text1}</p>
+        </div>
+
+        <div className="text-box2">
+            <p>{props.text2}</p>
+        </div>
+
+        <div className="text-box3">
+            <p>{props.text3}</p>
+        </div>
+
+        <div className="text-box4">
+            <p>{props.text4}</p>
+        </div>
+
+        <div className="text-box5">
+            <p>{props.text5}</p>
+        </div>
+
+        <div className="text-box6">
+            <p>{props.text6}</p>
+        </div>
+
+        <div className="text-box7">
+            <p>{props.text7}</p>
+        </div>
+
+        <div className="text-box8">
+            <p>{props.text8}</p>
+        </div>
+
+      </div>
     </div>
   )
 }
