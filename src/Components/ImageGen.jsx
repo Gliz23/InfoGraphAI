961c7fff715ce51e1bab23e386694858a23598a5
Gliz23 <geaakoto@st.ug.edu.gh>
2024-04-27 16:09:42 +0000

    ImageGen update Text4-7 addition

--- src/Components/ImageGen.jsx
+++ src/Components/ImageGen.jsx
@@ -3,6 +3,10 @@ import html2canvas from 'html2canvas';
 import Text1 from './BG Templates/Template1/Text1.jsx';
 import Text2 from './BG Templates/Template2/Text2.jsx';
 import Text3 from './BG Templates/Template3/Text3.jsx';
+import Text4 from './BG Templates/Template4/Text4.jsx';
+import Text5 from './BG Templates/Template5/Text5.jsx';
+import Text6 from './BG Templates/Template6/Text6.jsx';
+import Text7 from './BG Templates/Template7/Text7.jsx';
 import { getSentence } from './StoreSentences.jsx';
 
 
@@ -39,6 +43,14 @@ class ImageGen extends React.Component {
           <Text2 sentence = {sentence}/>
           <button className ={'fg-button'} onClick={this.captureImage}>Download Image</button>
           <Text3 sentence = {sentence}/>
+          <button className ={'fg-button'} onClick={this.captureImage}>Download Image</button>
+          <Text4 sentence = {sentence} />
+          <button className ={'fg-button'} onClick={this.captureImage}>Download Image</button>
+          <Text5 sentence = {sentence}/>
+          <button className ={'fg-button'} onClick={this.captureImage}>Download Image</button>
+          <Text6 sentence = {sentence}/>
+          <button className ={'fg-button'} onClick={this.captureImage}>Download Image</button>
+          <Text7 sentence = {sentence} />
           <button className ={'fg-button'} onClick={this.captureImage}>Download Image</button>
         </div>
       );
