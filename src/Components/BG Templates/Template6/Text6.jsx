83f9ef149320efe86771bd9a930ef2d7a8a78159
Gliz23 <geaakoto@st.ug.edu.gh>
2024-04-27 16:33:50 +0000

    Text6 file added

---
+++ src/Components/BG Templates/Template6/Text6.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import T6 from './T6.jsx';
+import imageUrl from '../../../assets/info-bg6.png';
+
+const Text6 = (props) => {
+   
+  return (
+        <div>
+          <h1 className='perfect-title'>Sixth InfoGraphic</h1>
+          <T6 
+          imageUrl={imageUrl}
+          text1={props.sentence[0]}
+          text2={props.sentence[1]}
+          text3={props.sentence[2]}
+          text4={props.sentence[3]}
+          text5={props.sentence[4]}
+          text6={props.sentence[5]}
+          text7={props.sentence[6]}
+          text8='InfoGraph AI'
+          />
+        </div>
+      );
+    };
+
+export default Text6
