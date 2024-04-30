8aa7d1740abf25783c8b5b8a6cd198d18dc6f9a1
Gliz23 <geaakoto@st.ug.edu.gh>
2024-04-30 17:38:25 +0000

    hero stlyes edit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,11 +7,10 @@ import { useNavigate } from 'react-router-dom'
 const Hero = () => {
   const navigate = useNavigate()
   return (
-    <div className= 'hero container'>
+    <div className= 'hero container' id='hero'>
       <div className='hero-text'>
         <h1> InfoGraphics to match your needs</h1>
         <p> Here, customizable infographics are made with quality in view</p>
-        <button className='button' onClick={() => navigate('/Create')}>Create</button>
       </div>
     </div>
   )
